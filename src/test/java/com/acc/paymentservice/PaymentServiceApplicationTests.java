45c90672300b0ebe35f0972bf8e93ef22ef32363
latif <latifirfansyah@gmail.com>
2024-08-30 07:39:27 +0700

    initial

---
+++ src/test/java/com/acc/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
